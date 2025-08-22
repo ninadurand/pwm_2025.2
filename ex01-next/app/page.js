61e8e976dcff6a2b49961f28ff535a6354b5bf40
Marina Mendes Durand <133005002+ninadurand@users.noreply.github.com>
2025-08-22 06:42:45 -0300

    Bio feita

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -3,9 +3,22 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-     oii
-     
-    </div>
-  );
+    <div style={{
+      display: "flex", 
+      flexDirection: "column", 
+      alignItems: "center", 
+      justifyContent: "center", 
+      height: "100vh",
+      fontFamily: "Arial"
+    }}>
+      <img 
+        src="/minha-foto.jpg" 
+        alt="Foto de Marina Durand" 
+        style={{ borderRadius: "50%", width: "150px", height: "150px" }} 
+      />
+      <h1>Marina Durand</h1>
+      <p>Meu nome é Marina Durand, sou estudante de Computação.</p>
+      <p>Gosto de aprender coisas novas e tenho interesse em design.</p>
+    </div>
+  );
 }
